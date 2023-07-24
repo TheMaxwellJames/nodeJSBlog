e50bfaa7a5b26a06b82e9945f7e7c5ca677b981c
Maxwell James <maxichanj@gmail.com>
2023-07-24 15:03:32 +0100

    first commit

---
+++ server/routes/main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+
+//Routes
+
+router.get('', (req, res) => {
+
+    const locals = {
+        title: "NodeJs Blog",
+        description: "Blog Created With NodeJs, Express And MongoDb"
+    }
+
+
+    res.render("index", { locals });
+});
+
+
+router.get('/about', (req, res) => {
+    res.render("about");
+});
+
+
+
+
+
+
+
+
+
+module.exports = router;
