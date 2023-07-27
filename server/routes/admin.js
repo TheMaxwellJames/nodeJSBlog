0e1d50bc261c92ea4d5b8f2fc1b844796a6e9921
Maxwell James <maxichanj@gmail.com>
2023-07-27 07:41:37 +0100

    admin login, midlleware and dashboard

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,15 +2,40 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post');
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 
 
 const adminLayout = '../views/layouts/admin';
+const jwtSecret = process.env.JWT_SECRET;
 
 
-//  Admin-Login Route
+/**
+ * 
+ * Check Login
+*/
+const authMiddleware = (req, res, next ) => {
+    const token = req.cookies.token;
+  
+    if(!token) {
+      return res.status(401).json( { message: 'Unauthorized'} );
+    }
+  
+    try {
+      const decoded = jwt.verify(token, jwtSecret);
+      req.userId = decoded.userId;
+      next();
+    } catch(error) {
+      res.status(401).json( { message: 'Unauthorized'} );
+    }
+  }
 
- router.get('/admin', async (req, res) => {
+
+//  Admin-Login Page
+
+ router.get('/admin',  async (req, res) => {
 
     try {
 
@@ -27,24 +52,61 @@ const adminLayout = '../views/layouts/admin';
 });
 
 
-// Admin Check Login
-
+/**
+ * POST /
+ * Admin - Check Login
+*/
 router.post('/admin', async (req, res) => {
-
     try {
-
-        const { username, password } = req.body;
-        console.log(req.body);
-
-
-
+      const { username, password } = req.body;
       
-        res.redirect('/admin');
-        
+      const user = await User.findOne( { username } );
+  
+      if(!user) {
+        return res.status(401).json( { message: 'Invalid credentials' } );
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if(!isPasswordValid) {
+        return res.status(401).json( { message: 'Invalid credentials' } );
+      }
+  
+      const token = jwt.sign({ userId: user._id}, jwtSecret );
+      res.cookie('token', token, { httpOnly: true });
+
+      res.redirect('/dashboard');
+  
     } catch (error) {
-        console.log(error);
+      console.log(error);
     }
-});
+  });
+
+
+
+  /**
+ * GET /
+ * Admin Dashboard
+*/
+router.get('/dashboard', authMiddleware,  async (req, res) => {
+    try {
+      const locals = {
+        title: 'Dashboard',
+        description: 'Simple Blog created with NodeJs, Express & MongoDb.'
+      }
+  
+      const data = await Post.find();
+      res.render('admin/dashboard', {
+        locals,
+        data,
+        layout: adminLayout
+      });
+  
+    } catch (error) {
+      console.log(error);
+    }
+  
+  });
 
 
 
@@ -58,6 +120,41 @@ router.post('/admin', async (req, res) => {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Admin  register
+
+router.post('/register', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      try {
+        const user = await User.create({ username, password:hashedPassword });
+        res.status(201).json({ message: 'User Created', user });
+      } catch (error) {
+        if(error.code === 11000) {
+          res.status(409).json({ message: 'User already in use'});
+        }
+        res.status(500).json({ message: 'Internal server error'})
+      }
+  
+    } catch (error) {
+      console.log(error);
+    }
+  });
 
 
 
